ba1b346f3295e603265fa30d9dfb0e4dfcdeae3c
wwzerp <mendesdacostabastos@gmail.com>
2025-11-19 23:22:07 +0000

    test4

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-// Espera que o documento carregue
 document.addEventListener('DOMContentLoaded', function() {
     
     const form = document.getElementById('contactForm');
 
     if (form) {
         form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Impede o envio real para não recarregar a página
+            event.preventDefault(); 
             
             const nome = document.getElementById('nome').value;
             
-            // Simples alerta de sucesso
             alert(`Obrigado, ${nome}! A sua mensagem foi enviada para a NYSE. Entraremos em contacto brevemente.`);
             
-            form.reset(); // Limpa o formulário
+            form.reset();
         });
     }
 });
